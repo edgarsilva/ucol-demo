47c971e05cf5732b39c93f610314b2c5910891d9
edgarsilva <edgar.osc@gmail.com>
2022-10-15 10:37:00 -0500

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Next.js Hello World!
         </h1>
 
         <p className={styles.description}>
@@ -22,33 +25,21 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+            <h1 className="text-8xl text-center">{counter}</h1>
+            <button
+              class="h-10 px-6 mx-4 font-semibold rounded-md bg-black text-white"
+              onClick={ () => setCounter(counter - 1) }
+            >
+              -
+            </button>
+            <button
+              class="h-10 px-6 mx-4 font-semibold rounded-md bg-black text-white"
+              onClick={ () => setCounter(counter + 1) }
+            >
+              +
+            </button>
+          </div>
         </div>
       </main>
 
